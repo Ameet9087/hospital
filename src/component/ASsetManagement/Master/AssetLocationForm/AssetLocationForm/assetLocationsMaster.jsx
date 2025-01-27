03d2d14b5da05e2077788c196d3dcfdc6718751b
LopMudraHS <md.ubaid007@gmail.com>
2025-01-27 15:13:17 +0530

    Arbaz

--- src/component/ASsetManagement/Master/AssetLocationForm/AssetLocationForm/assetLocationsMaster.jsx
+++ src/component/ASsetManagement/Master/AssetLocationForm/AssetLocationForm/assetLocationsMaster.jsx
@@ -1,7 +1,7 @@
 import React, { useState, useRef, useEffect } from "react";
 import "./assetLocationsMaster.css";
 import AssetLocationsMasterPopUp from "./assetLocationsMasterPopUp";
-import { startResizing } from "../../../../TableHeadingResizing/ResizableColumns";
+import { startResizing } from "../../../../../TableHeadingResizing/ResizableColumns";
 import CustomModal from "../../../../CustomModel/CustomModal";
 import { API_BASE_URL } from "../../../../api/api";
 
