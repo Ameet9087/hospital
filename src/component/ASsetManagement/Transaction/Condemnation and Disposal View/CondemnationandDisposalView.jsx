aefd510d676bd3cdad7f6492760228819735cbfd
LopMudraHS <md.ubaid007@gmail.com>
2025-01-27 15:10:57 +0530

    Arbaz

--- src/component/ASsetManagement/Transaction/Condemnation and Disposal View/CondemnationandDisposalView.jsx
+++ src/component/ASsetManagement/Transaction/Condemnation and Disposal View/CondemnationandDisposalView.jsx
@@ -1,6 +1,6 @@
 import React, { useState, useRef, useEffect } from "react";
 import "./CondemnationandDisposalView.css";
-import { startResizing } from "../../../TableHeadingResizing/resizableColumns";
+import { startResizing } from "../../../../TableHeadingResizing/ResizableColumns";
 import CustomModal from "../../../CustomModel/CustomModal";
 import * as XLSX from "xlsx";
 import { API_BASE_URL } from "../../../api/api";
