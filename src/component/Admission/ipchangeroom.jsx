15d6627ddee2e024d33f5295d471c6abed00bbbe
LopMudraHS <md.ubaid007@gmail.com>
2025-01-28 21:40:12 +0530

    Arbaz

--- src/component/Admission/ipchangeroom.jsx
+++ src/component/Admission/ipchangeroom.jsx
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from "react";
-import "./ipchangeroom.css";
+import "./Ipchangeroom.css";
 import PopupTable from "../Admission/PopupTable";
 import { FaSearch } from "react-icons/fa";
 import axios from "axios";
