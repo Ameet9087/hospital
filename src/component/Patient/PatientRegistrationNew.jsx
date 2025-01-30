1e3d4fab44f386b1baacfc0e374a14b374f7755c
LopMudraHS <md.ubaid007@gmail.com>
2025-01-30 18:26:13 +0530

    Arbaz

--- src/component/Patient/PatientRegistrationNew.jsx
+++ src/component/Patient/PatientRegistrationNew.jsx
@@ -1207,11 +1207,11 @@ const PatientRegistrationNew = ({ onClose }) => {
                     />
                   </td>
                   <td>
-                    <textarea
+                    <input
                       type="text"
                       value={doctor.residenceAddress}
                       placeholder="Address"
-                      className="patient-registration-component-textarea"
+                      className="patient-registration-component"
                     />
                   </td>
                   <td>
