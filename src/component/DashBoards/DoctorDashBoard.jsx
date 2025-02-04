3b61a9ffb4200330e321fe298a05e7f72c1bb4cd
LopMudraHS <md.ubaid007@gmail.com>
2025-02-04 16:28:09 +0530

    Arbaz

--- src/component/DashBoards/DoctorDashBoard.jsx
+++ src/component/DashBoards/DoctorDashBoard.jsx
@@ -12,6 +12,7 @@ import InPatient from "./InPatient";
 import Records from "./PatientsRecord";
 import DoctorMainPage from "./OpDoctorMainPage";
 import IpDoctorMainPage from "./IpDoctorMainPage";
+import DoctorPrescription from "../DashBoards/DoctorPrescription/DoctorPrescription"
 
 const DrDashboard = () => {
   const location = useLocation();
@@ -22,20 +23,18 @@ const DrDashboard = () => {
       <div className="doctor-dashboard-button-group">
         <Link to="/doctor/outpatient">
           <button
-            className={`dashboard-button ${
-              location.pathname === "/doctor/outpatient" ? "active" : ""
-            }`}
+            className={`dashboard-button ${location.pathname === "/doctor/outpatient" ? "active" : ""
+              }`}
           >
             Out Patient
           </button>
         </Link>
         <Link to="/doctor/inpatientdepartment">
           <button
-            className={`dashboard-button ${
-              location.pathname === "/doctor/inpatientdepartment"
-                ? "active"
-                : ""
-            }`}
+            className={`dashboard-button ${location.pathname === "/doctor/inpatientdepartment"
+              ? "active"
+              : ""
+              }`}
           >
             In Patient Department
           </button>
@@ -49,6 +48,14 @@ const DrDashboard = () => {
             Patient Record
           </button>
         </Link> */}
+        <Link to="/doctor/prescription">
+          <button
+            className={`dashboard-button ${location.pathname === "/doctor/prescription" ? "active" : ""
+              }`}
+          >
+            Prescription
+          </button>
+        </Link>
       </div>
 
       {/* Content Rendering */}
@@ -56,6 +63,7 @@ const DrDashboard = () => {
         <Routes>
           <Route path="/outpatient" element={<DoctorMainPage />} />
           <Route path="/inpatientdepartment" element={<IpDoctorMainPage />} />
+          <Route path="/prescription/*" element={<DoctorPrescription />} />
           {/* <Route path="/patientrecord" element={<Records />} /> */}
         </Routes>
       </div>
